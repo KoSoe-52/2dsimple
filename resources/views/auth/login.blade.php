895e606d030384701f81ad899db05fe311011fb5
DESKTOP-2K6F7VH\Ko Soe <soelay211108@gmail.com>
2022-05-13 04:01:31 +0630

    login form design changed

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,10 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-
 	<title>Signin</title>
-	
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
@@ -12,32 +9,30 @@
 	<meta name="description" content="" />
 	<meta name="keywords" content="">
 	<meta name="author" content="itBrother" />
-	<!-- Favicon icon -->
-	<link rel="icon" href="{{asset('assets/images/favicon.ico')}}" type="image/x-icon">
-
+	<link rel="icon" href="{{asset('assets/images/logo.png')}}" type="image/x-icon">
 	<!-- vendor css -->
 	<link rel="stylesheet" href="{{asset('assets/css/style.css')}}">
 </head>
-
+<body style="overflow:hidden;">
 <!-- [ auth-signin ] start -->
-<div class="auth-wrapper">
+<div class="auth-wrapper" style="background:#3C4857 !important">
 	<div class="auth-content">
-		<div class="card">
-			<div class="row align-items-center text-center">
-				<div class="col-md-12">
+		<div class="card"  style="margin-top:-100px;">
+			<div class="row align-items-center text-center" >
+				<div class="col-md-12" >
 					<div class="card-body">
-					<p style="color:green;">{{Auth::logout()}} </p>
-                                @error('error')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                        <form method="POST" action="{{ route('login') }}">
+						<p style="color:green;">{{Auth::logout()}} </p>
+							@error('error')
+								<span class="invalid-feedback" role="alert">
+									<strong>{{ $message }}</strong>
+								</span>
+							@enderror
+                        <form method="POST" action="{{ route('login') }}" >
 							@csrf
-							<img src="{{asset('assets/images/adlogo.png')}}" alt="" class="img-fluid mb-4">
-							<h3 class="mb-3 f-w-400 text-c-green">SignIn</h3>
-							<div class="form-group mb-3">
-								<label class="floating-label text-dark" for="login">Username</label>
+							<img src="{{asset('assets/images/logo.png')}}" alt="" style="width:60px;height:60px;" class="img-fluid mb-1">
+							<h3 class="mb-2 f-w-400 text-c-green">SignIn</h3>
+							<div class="form-group mb-4">
+								<label class="floating-label text-dark" for="login">လျှို့ဝှက်အမည်</label>
 								<input id="login" type="text"
 									class="form-control{{ $errors->has('name') || $errors->has('email') ? ' is-invalid' : '' }}"
 									name="login" value="{{ old('name') ?: old('email') }}" autocomplete="off"  required>
@@ -47,8 +42,8 @@
 										</span>
 									@endif
 							</div>
-							<div class="form-group mb-4">
-								<label class="floating-label text-dark" for="Password">Password</label>
+							<div class="form-group  mb-3">
+								<label class="floating-label text-dark" for="Password">လျှို့ဝှက်ကုဒ်</label>
 								<input id="password" type="password" class="form-control @error('password') is-invalid @enderror" autocomplete="off" name="password" required autocomplete="off">
 									@error('password')
 										<span class="invalid-feedback" role="alert">
@@ -56,12 +51,12 @@
 										</span>
 									@enderror
 							</div>
-							<div class="custom-control custom-checkbox text-left mb-4 mt-2">
+							<div class="custom-control custom-checkbox text-left mb-2 mt-2">
 								<input type="checkbox" class="custom-control-input" id="customCheck1">
 								<!-- <label class="custom-control-label" for="customCheck1">Save credentials.</label> -->
 							</div>
 							
-							<div><button type="submit" class="btn btn-block btn-primary mb-4">Signin</button></div>
+							<div><button type="submit" class="btn btn-block btn-primary mb-4">ဝင်မည်</button></div>
 							</form>
 							<!-- <p class="mb-2 text-muted">Forgot password? <a href="{{url('/login')}}" class="f-w-400">Reset</a></p>
 							<p class="mb-0 text-muted">Don’t have an account? <a href="{{url('/logout')}}" class="f-w-400">Signup</a></p>
