bd80835d7f7daedba0cd10e117b9d8cfdb4808fd
DESKTOP-2K6F7VH\Ko Soe <soelay211108@gmail.com>
2022-05-18 11:10:51 +0630

    changed line loader

--- resources/views/layouts/moderator.blade.php
+++ resources/views/layouts/moderator.blade.php
@@ -12,7 +12,7 @@
 <link rel="icon" href="{{asset('assets/images/logo.png')}}" type="image/x-icon">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
 <link href="{{asset('css/moderator.css')}}" rel="stylesheet">
-<!-- <link rel="stylesheet" href="{{asset('css/load.css')}}"> -->
+<link rel="stylesheet" href="{{asset('css/load.css')}}">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
  @yield('style')
 </head>
@@ -32,7 +32,7 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
     <script src="{{asset('js/moderator.js')}}"></script>
-    <!-- <script src="{{asset('assets/js/pcoded.min.js')}}"></script> -->
+    <script src="{{asset('assets/js/pcoded.min.js')}}"></script>
 
     @yield('script')
 </body>
