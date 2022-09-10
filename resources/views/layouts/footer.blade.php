8f32bec48c14b243711c7b84d0b70cf90ebc870c
DESKTOP-2K6F7VH\Ko Soe <soelay211108@gmail.com>
2022-09-10 17:04:42 +0630

    footer fixed

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -14,16 +14,14 @@
         </div>
     </nav>
 </div> -->
-<nav class="navbar navbar-inverse bg-dark" style="background-color:red;width:100% !important;position:fixed;bottom:0">
-  <div class="container-fluid">
-    <div style="display: inline-block;width:100%;text-align:center;">
+<nav class="navbar navbar-inverse bg-dark" style="width:100% !important;position:fixed;bottom:0">
+  <div class="container-fluid" style="display: inline-block;width:100%;padding:0;">
       <ul class="nav" style="width:100% !important;text-align:center;">
-        <li style="padding:5px 5px 5px 0px;" class="menu 2d twod"><a href="{{ url('2d') }}" style="padding-left:0px">  Thai2D</a></li>
-        <li style="padding:5px 5px 5px 0px;" class="menu dubai2d"><a href="{{ url('dubai2d') }}" style="padding-left:5px">  Dubai2D</a></li>
-        <li style="padding:5px 5px 5px 0px;" class="menu histories"><a href="{{ url('histories') }}" style="padding-left:5px"> History</a></li>
-        <li style="padding:5px 5px 5px 0px;" class="menu"><a href="{{ route('logout') }}" style="padding-left:5px"> Logout</a></li>
+        <li style="padding:5px 5px 5px 0px;text-align:left;" class="menu 2d twod"><a href="{{ url('2d') }}" style="padding-left:0px">Thai2D</a></li>
+        <li style="padding:5px 5px 5px 0px;text-align:left;" class="menu dubai2d"><a href="{{ url('dubai2d') }}" style="padding-left:0px">  Dubai2D</a></li>
+        <li style="padding:5px 5px 5px 0px;text-align:left;" class="menu histories"><a href="{{ url('histories') }}" style="padding-left:4px"> History</a></li>
+        <li style="padding:5px 5px 5px 0px;text-align:left;" class="menu"><a href="{{ route('logout') }}" style="padding-left:0px"> Logout</a></li>
       </ul>
-    </div>
   </div>
 </nav>
 <style>
@@ -32,6 +30,6 @@ a:visited { text-decoration: none; }
 a:hover { text-decoration: none; }
 a:active { text-decoration: none; }
 .menu{
-  width:25%;
+  width:25% !important;
 }
 </style>
